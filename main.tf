aaefd99e28c28b8bd66ac142761693f874b215fc
anulithavardhan <107521881+anulithavardhan@users.noreply.github.com>
2023-04-24 17:19:01 -0400

    Function App

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ resource "random_integer" "deployment_id_suffix" {
 // Resource Group
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.class_name}-amarripe-${var.environment}-02-rg"
+  name     = "${var.class_name}-amarripe-${var.environment}-03-rg"
   location = var.location
 
   tags = local.tags
@@ -50,7 +50,7 @@ resource "azurerm_application_insights" "example" {
 }
 
 resource "azurerm_key_vault" "example" {
-  name                = "anu-workspacevault-new2"
+  name                = "anu-workspacevault-new3"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tenant_id           = data.azurerm_subscription.current.tenant_id
@@ -133,4 +133,12 @@ resource "azurerm_app_service" "webapp" {
 
 output "webapp_url" {
   value = azurerm_app_service.webapp.default_site_hostname
+}
+resource "azurerm_function_app" "function_app" {
+  name                       = "amarripe-function-app"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  app_service_plan_id        = azurerm_app_service_plan.webapp_plan.id
+  storage_account_name       = azurerm_storage_account.storage.name
+  storage_account_access_key = azurerm_storage_account.storage.primary_access_key
 }
